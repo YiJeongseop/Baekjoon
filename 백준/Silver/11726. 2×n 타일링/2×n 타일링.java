b2af4bc2a7c64d4081f20287826ffb0c0815450e
Jeongseop Lee <112690335+LeeJeongseop@users.noreply.github.com>
2022-10-13 16:28:04 +0900

    [Silver III] Title: 2×n 타일링, Time: 132 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static int dp[];
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		dp = new int[N + 2];
+		dp[1] = 1;
+		dp[2] = 2;
+		
+		for(int i = 3; i <= N; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2])%10007;
+		}
+		
+		bw.write(dp[N]+"");
+		bw.flush();
+		bw.close();
+	}
+}
