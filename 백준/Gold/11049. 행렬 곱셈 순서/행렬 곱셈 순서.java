6f1bee4c0800922848c0b9166ceeee39563dfb19
Jeongseop Lee <112690335+LeeJeongseop@users.noreply.github.com>
2022-10-15 16:21:32 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 316 ms, Memory: 18604 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -21,10 +21,6 @@ public class Main {
 			RC[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		for(int i = 1; i <= N; i++) {
-			M[i][i] = 0;
-		}
-		
 		for(int i = 1; i <= N - 1; i++) {
 			for(int j = 1; j <= N - i; j++) {
 				M[j][j + i] = Integer.MAX_VALUE;
