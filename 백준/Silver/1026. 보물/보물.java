c2399b1e31b74dc98ec90cf7d63a7389c1cf6610
Jeongseop Lee <112690335+LeeJeongseop@users.noreply.github.com>
2022-11-01 13:03:04 +0900

    [Silver IV] Title: 보물, Time: 128 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main (String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		int[] B = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			B[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		int result = 0;
+		for(int i = 0; i < N; i++) {
+			result += A[N - i - 1] * B[i];
+		}
+		
+		bw.write(result + "");
+		bw.flush();
+		bw.close();
+	}
+}
