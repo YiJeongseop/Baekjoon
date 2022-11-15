bc09e6c3c215adf3f6fce8909be5afa7fe6bebc5
Jeongseop Lee <112690335+LeeJeongseop@users.noreply.github.com>
2022-11-15 15:54:17 +0900

    [Silver III] Title: N과 M (6), Time: 148 ms, Memory: 15900 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, M;
+	static int[] arr, result;
+	static boolean[] visited;
+	static BufferedWriter bw;
+	
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		result = new int[M];
+		visited = new boolean[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(arr);
+		
+		backtracking(0);
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void backtracking(int level) throws IOException{
+		if(level == M) {
+			for(int i = 0; i < M; i ++) bw.write(result[i] + " ");
+			bw.write("\n");
+			return;
+		}
+		
+		for(int i = 1; i < N + 1; i++) {
+			if(visited[i - 1] == true) continue;
+			if(level != 0) {
+				if(result[level - 1] >= arr[i - 1]) continue;
+			}
+			result[level] = arr[i - 1];
+			visited[i - 1] = true;
+			backtracking(level + 1);
+			visited[i - 1] = false;
+		}
+	}
+} 
