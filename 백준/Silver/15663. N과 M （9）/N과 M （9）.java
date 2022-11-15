6187f874bf1fb56f273cc2be849dbd8e4fc7f7dc
Jeongseop Lee <112690335+LeeJeongseop@users.noreply.github.com>
2022-11-15 16:28:00 +0900

    [Silver II] Title: N과 M (9), Time: 448 ms, Memory: 56848 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+import javax.management.monitor.MonitorSettingException;
+
+public class Main {
+	
+	static int N, M;
+	static int[] arr, result;
+	static boolean[] visit;
+	static BufferedWriter bw;
+	static StringBuilder sb = new StringBuilder();
+	static HashSet<String> set = new HashSet<String>();
+	
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		result = new int[M];
+		visit = new boolean[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(arr);
+		
+		backtracking(0);
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void backtracking(int level) throws IOException{
+		if(level == M) {
+			StringBuilder sb2 = new StringBuilder();
+			for(int i = 0; i < M; i ++) sb2.append(result[i] + " ");
+			if(!set.contains(sb2.toString())) {
+				sb.append(sb2.toString()).append("\n");
+				set.add(sb2.toString());
+			}
+			return;
+		}
+		
+		for(int i = 1; i < N + 1; i++) {
+			if(visit[i - 1] == true) continue;
+			result[level] = arr[i - 1];
+			visit[i - 1] = true;
+			backtracking(level + 1);
+			visit[i - 1] = false;
+		}
+	}
+} 
