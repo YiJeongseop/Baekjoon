ce639214bc0b6a6900dbca1afac1840bcd454b87
Jeongseop Lee <112690335+LeeJeongseop@users.noreply.github.com>
2022-11-15 15:15:57 +0900

    [Silver III] Title: N과 M (3), Time: 572 ms, Memory: 157992 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, M;
+	static int[] arr;
+	static BufferedWriter bw;
+	
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[M];
+		
+		backtracking(0);
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void backtracking(int level) throws IOException{
+		if(level == M) {
+			for(int i = 0; i < M; i ++) bw.write(arr[i] + " ");
+			bw.write("\n");
+			return;
+		}
+		
+		for(int i = 1; i < N + 1; i++) {
+			arr[level] = i;
+			backtracking(level + 1);
+			arr[level] = 0;
+		}
+	}
+} 
