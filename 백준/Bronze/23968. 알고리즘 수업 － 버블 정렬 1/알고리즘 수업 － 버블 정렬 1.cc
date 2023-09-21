87c8825094bc3c4773288d6ffae27bfbd70087b4
Jeongseop Yi <linko1a2b@gmail.com>
2023-09-22 07:11:25 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	int* array = new int[n];
+
+	int temp;
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+		array[i] = temp;
+	}
+
+	int cnt = 0;
+	for (int i = 0; i < n - 1; i++) {
+		bool swapped = false;
+
+		for (int j = 0; j < n - 1; j++) {
+			if (array[j] > array[j + 1]) {
+				swap(array[j], array[j + 1]);
+				swapped = true;
+				cnt++;
+				if (cnt == k) {
+					cout << array[j] << " " << array[j + 1];
+				}
+			}
+		}
+
+		if (swapped == false)
+			break;
+	}
+
+	if (cnt < k) {
+		cout << -1;
+	}
+
+	delete array;
+}
