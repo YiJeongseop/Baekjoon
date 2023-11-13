a02c636c8ed0aa95cadff4379eb38f94466d5411
Jeongseop Yi <linko1a2b@gmail.com>
2023-11-13 20:03:02 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string arr[5];
+
+	for (int i = 0; i < 5; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (i < arr[j].length()) {
+				cout << arr[j][i];
+			}
+		}
+	}
+
+	return 0;
+}
