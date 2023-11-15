d6c379d21863b741850b99d644d77b2e97aedc20
Jeongseop Yi <linko1a2b@gmail.com>
2023-11-16 05:02:20 +0900

    [Bronze I] Title: 소인수분해, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	if (n == 1) return 0;
+
+	while (n != 1) {
+		for (int i = 2; i <= n; i++) {
+			if (n % i == 0) {
+				cout << i << '\n';
+				n /= i;
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
