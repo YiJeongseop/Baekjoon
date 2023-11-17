7e0948a80076fdc1cdc39cb32223479f18249a94
Jeongseop Yi <linko1a2b@gmail.com>
2023-11-17 15:14:03 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + n);
+
+	cout << arr[n - k];
+
+	delete[] arr;
+
+	return 0;
+}
