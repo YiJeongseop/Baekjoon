3e7aed080af1a145e923ddf14c91ccdc228ae349
Jeongseop Yi <linko1a2b@gmail.com>
2023-11-17 15:10:02 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + n);
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << '\n';
+	}
+
+	delete[] arr;
+
+	return 0;
+}
