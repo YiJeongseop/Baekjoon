04de0eeab2d495d5d468b71ec17ec85bda860124
Jeongseop Yi <linko1a2b@gmail.com>
2023-12-24 08:24:35 +0900

    [Bronze II] Title: 숫자, Time: 16 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	unsigned long long a, b;
+	
+	cin >> a >> b;
+
+	if (a == b) {
+		cout << 0;
+	}
+	else if (a < b) {
+		cout << b - a - 1 << '\n';
+
+		for (unsigned long long i = a + 1; i < b; i++) {
+			cout << i << " ";
+		}
+	}
+	else {
+		cout << a - b - 1 << '\n';
+
+		for (unsigned long long i = b + 1; i < a; i++) {
+			cout << i << " ";
+		}
+	}
+
+	return 0;
+}
