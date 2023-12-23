09f8d2e0451b8d39aa1d1a404a09c44106abec8e
Jeongseop Yi <linko1a2b@gmail.com>
2023-12-24 08:49:49 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < i - 1; j++) {
+			cout << " ";
+		}
+		for (int j = 0; j < n * 2 - (i * 2 - 1); j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
